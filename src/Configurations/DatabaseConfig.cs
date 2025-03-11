a90e1675e19c16de7611e84d763d43e5d6070125
ilyan <ilyan.fraimbault@etu.umontpellier.fr>
2025-03-11 18:51:37 +0100

    refactor: add AllowUserVariables option to connection string in DatabaseConfig

--- src/Configurations/DatabaseConfig.cs
+++ src/Configurations/DatabaseConfig.cs
@@ -4,7 +4,7 @@ namespace OTPBUILD.Configurations;
 
 public class DatabaseConfig(string server, string uid, string pwd, string database)
 {
-    private readonly string _connectionString = $"Server={server};UserID={uid};Password={pwd};Database={database}";
+    private readonly string _connectionString = $"Server={server};UserID={uid};Password={pwd};Database={database};AllowUserVariables=True;";
 
     public MySqlConnection GetConnection()
     {
